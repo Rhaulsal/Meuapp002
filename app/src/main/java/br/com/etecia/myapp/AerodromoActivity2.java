9c2edadb27a43ba772a974456b7349b8d93c9e3f
Rhaulsal <Rhaulsal2007@gmail.com>
2023-08-31 22:03:59 -0300

    página interligada

---
+++ app/src/main/java/br/com/etecia/myapp/AerodromoActivity2.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AerodromoActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aerodromo_layout);
+    }
+}
