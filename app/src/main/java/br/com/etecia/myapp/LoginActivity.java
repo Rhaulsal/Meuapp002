d3c29dac9045432ab8341fe011050db66236655e
Rhaulsal <rhaulsal2007@gmail.com>
2023-09-14 22:13:44 -0300

    Interligando

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
     ImageView imgFoto;
+    Button btnEntrar;
 
 
     @Override
--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity2.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity2.java
@@ -3,12 +3,24 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MenuPrincipalActivity2 extends AppCompatActivity {
-
+    MaterialCardView cardNavios;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal_layout);
+        cardNavios = findViewById(R.id.cardNavios);
+
+        cardNavios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Cliquei em Galeria",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
