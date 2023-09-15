40fa23a3820111ea0a0baeca00f65f8ddd48ef30
Rhaulsal <rhaulsal2007@gmail.com>
2023-09-14 21:51:43 -0300

    Interligando

--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -2,13 +2,43 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    //declarar a variável global do tipo objeto no layout
+    ImageView imgFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
+        imgFoto = findViewById(R.id.imgFoto);
+       //criando evento de click na imagem
+        imgFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
